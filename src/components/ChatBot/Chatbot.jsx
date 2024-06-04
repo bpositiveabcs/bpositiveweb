f2bede08836f750bd85354968478312709a6c726
BiaBiuta <bianca.furtos17@gmail.com>
2024-06-04 08:37:28 +0300

    chat button

--- src/components/ChatBot/Chatbot.jsx
+++ src/components/ChatBot/Chatbot.jsx
@@ -16,7 +16,7 @@ const Chatbot = () => {
         const userMessage = { sender: 'user', text: message };
 
         try {
-            const res = await fetch('http://localhost:5000/chatbot', {
+            const res = await fetch('http://localhost:55555/chatbot', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message }),
